c5223fc66b6ea770e3c48113ca6770aec5f79f23
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-08-18 17:17:53 +0200

    Workin on the ABout Me section of my page

--- src/components/About_Me/AboutMe.tsx
+++ src/components/About_Me/AboutMe.tsx
@@ -6,7 +6,9 @@ const AboutMe = () => {
       <section className="bg-primary md:px-20 px-2 md:py-12 py-8 grid md:grid-cols-2 grid-cols-1 md:gap-8 gap-14 w-full place-items-center">
         <div className="w-full ">
           <div className="w-full">
-            <h1 className="text-3xl text-text pb-2">About Me</h1>
+            <h1 className="md:text-4xl text-3xl text-text md:pb-4 pb-2">
+              About Me
+            </h1>
             <p className="text-text pb-6">
               As a passionate Front-end React Developer, I bring ideas to life
               through clean, efficient code. With a keen eye for design and a
@@ -23,21 +25,24 @@ const AboutMe = () => {
             ))}
           </div>
         </div>
-        <div className="w-full bg-background text-text">
+        {/*//? contet two  */}
+        <div className="w-full bg-background text-text md:px-10 px-5 md:py-18 py-10 rounded-md">
           <code>
             const AboutMe = () =&gt; <br />
             {<code>const [passion, setPassion] = useState("coding")</code>}{" "}
+            <br />
             <br />
             <br />
             <code>
               useEffect(() =&gt; <br /> setPassion("creating amazing web
               experiences") <br /> , []) <br />
               <br />
+              <br />
               return(
               <br />
               <br />
               Hello, I am Oplano James Mulbah, <br />
-              <br />I passionate Front-end React <br />
+              <br />I passionate about Front-end React <br />
               <br /> );
             </code>
           </code>
